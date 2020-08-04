b146a4bc304a3e42e57ed8f0fbcfb91e9ad1de1a
andy99ge <zjdecjtu@163.com>
2020-08-04 11:48:55 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myapp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
